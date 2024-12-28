d4d0482dd022bf846af6a643eb5285db49866169
hjhessmann <hans.joerg.hessmann@anderscore.com>
2024-12-28 23:05:22 +0100

    Add validation

--- spring/soap/spring-ws-countries-full/src/main/java/com/example/countries/WebServiceConfig.java
+++ spring/soap/spring-ws-countries-full/src/main/java/com/example/countries/WebServiceConfig.java
@@ -1,18 +1,29 @@
 package com.example.countries;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+
+import java.util.List;
 
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Value("classpath:/wsdl/countries.wsdl")
+    private Resource wsdlResource;
+
+    @Value("classpath:/xsd/countries.xsd")
+    private Resource xsdResource;
 
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
@@ -24,6 +35,20 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 
     @Bean
     public SimpleWsdl11Definition countries() {
-        return new SimpleWsdl11Definition(new ClassPathResource("/wsdl/countries.wsdl"));
+        return new SimpleWsdl11Definition(wsdlResource);
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(payloadValidatingInterceptor());
+    }
+
+    @Bean
+    public PayloadValidatingInterceptor payloadValidatingInterceptor() {
+        var interceptor = new PayloadValidatingInterceptor();
+        interceptor.setSchema(xsdResource);
+        interceptor.setValidateRequest(true);
+        interceptor.setValidateResponse(true);
+        return interceptor;
     }
 }
