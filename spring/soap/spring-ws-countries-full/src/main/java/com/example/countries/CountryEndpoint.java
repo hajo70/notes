7d6cd91027df4f3c52148f8a342fa5e3a5f55aba
hjhessmann <hans.joerg.hessmann@anderscore.com>
2024-12-28 22:20:09 +0100

    Add Header

--- spring/soap/spring-ws-countries-full/src/main/java/com/example/countries/CountryEndpoint.java
+++ spring/soap/spring-ws-countries-full/src/main/java/com/example/countries/CountryEndpoint.java
@@ -2,14 +2,22 @@ package com.example.countries;
 
 import com.example.countries.wsdl.GetCountryRequest;
 import com.example.countries.wsdl.GetCountryResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapHeaderElement;
 import org.springframework.ws.soap.server.endpoint.annotation.SoapAction;
+import org.springframework.ws.soap.server.endpoint.annotation.SoapHeader;
+
+import java.lang.invoke.MethodHandles;
 
 @Endpoint
 public class CountryEndpoint {
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     private final CountryRepository countryRepository;
 
     @Autowired
@@ -19,7 +27,10 @@ public class CountryEndpoint {
 
     @SoapAction("http://spring.io/guides/gs-producing-web-service/GetCountryRequest")
     @ResponsePayload
-    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request,
+                                         @SoapHeader("{http://spring.io/guides/gs-producing-web-service}User") SoapHeaderElement user) {
+        log.info("getCountry for User {}", user.getText());
+
         GetCountryResponse response = new GetCountryResponse();
         response.setCountry(countryRepository.findCountry(request.getName()));
 
--- spring/soap/spring-ws-countries-full/src/test/java/com/example/countries/CountryClient.java
+++ spring/soap/spring-ws-countries-full/src/test/java/com/example/countries/CountryClient.java
@@ -4,8 +4,14 @@ import com.example.countries.wsdl.GetCountryRequest;
 import com.example.countries.wsdl.GetCountryResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapHeaderElement;
+import org.springframework.ws.soap.SoapMessage;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
 
 public class CountryClient extends WebServiceGatewaySupport {
 
@@ -19,11 +25,22 @@ public class CountryClient extends WebServiceGatewaySupport {
         log.info("Requesting location for " + country);
 
         GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
-                .marshalSendAndReceive(request,
-                        new SoapActionCallback(
-                                "http://spring.io/guides/gs-producing-web-service/GetCountryRequest"));
-
+                .marshalSendAndReceive(request, new HeaderCallback());
         return response;
     }
 
+    static class HeaderCallback extends SoapActionCallback {
+
+        HeaderCallback() {
+            super("http://spring.io/guides/gs-producing-web-service/GetCountryRequest");
+        }
+
+        @Override
+        public void doWithMessage(WebServiceMessage message) throws IOException {
+            super.doWithMessage(message);
+            SoapMessage soapMessage = (SoapMessage) message;
+            SoapHeaderElement userElement = soapMessage.getSoapHeader().addHeaderElement(new QName("http://spring.io/guides/gs-producing-web-service", "User"));
+            userElement.setText("Hugo");
+        }
+    }
 }
